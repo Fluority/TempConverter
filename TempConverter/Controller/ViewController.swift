d5f82285b09d8df98b4bf3b832c9e44c7d1852cd
Lucyfer <liene.krista@gmail.com>
2023-04-16 23:07:41 +0300

    Initial commit

---
+++ TempConverter/Controller/ViewController.swift
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+//  ViewController.swift
+//  TempConverter
+//  Created liene.krista.neimane on 14/04/2023. on 14/04/2023.
+
+=======
+>>>>>>> parent of 302da8f (.)
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var tempSlider: UISlider!{
+        didSet{
+            tempSlider.maximumValue = 100
+            tempSlider.minimumValue = 0
+            tempSlider.value = 0
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        celsiusLabel.text = "32 Fº"
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+//        print("tempSlider:", tempSlider.value)
+        updateResultLabelValue(value: tempSlider.value)
+    }
+    
+    
+    @IBAction func segmentValueChanged(_ sender: Any) {
+        updateResultLabelValue(value: tempSlider.value)
+    }
+    
+    func updateResultLabelValue(value: Float) {
+        print(value)
+        
+        let celsiusTemp = Int(value)
+        celsiusLabel.text = "\(celsiusTemp) Cº"
+        
+        var convertedTempString = ""
+        #warning("switch case, convertedTempString = convertTempFrom(celsius: celsiusTemp)")
+    
+    }
+    
+
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double) {
+        #warning("temp calculation logic")
+        return (0,0)
+    }
+
+}
+
