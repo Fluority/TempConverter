cf8120d08037e1dcbc43edd3560033aaf41ebd8c
Lucyfer <liene.krista@gmail.com>
2023-04-17 12:38:34 +0300

    Created the conversion, added kelvin

--- TempConverter/Controller/ViewController.swift
+++ TempConverter/Controller/ViewController.swift
@@ -12,46 +12,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBOutlet weak var tempSlider: UISlider!{
-        didSet{
+    @IBOutlet weak var tempSlider: UISlider! {
+        didSet {
             tempSlider.maximumValue = 100
             tempSlider.minimumValue = 0
             tempSlider.value = 0
         }
     }
     
+    @IBOutlet weak var tempSegment: UISegmentedControl! // Add this IBOutlet to connect to the segmented control in the storyboard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        celsiusLabel.text = "32 Fº"
-    }
-    
-    @IBAction func sliderValueChanged(_ sender: Any) {
-//        print("tempSlider:", tempSlider.value)
+        celsiusLabel.text = "Cº"
         updateResultLabelValue(value: tempSlider.value)
     }
     
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        updateResultLabelValue(value: tempSlider.value)
+    }
     
     @IBAction func segmentValueChanged(_ sender: Any) {
         updateResultLabelValue(value: tempSlider.value)
     }
     
     func updateResultLabelValue(value: Float) {
-        print(value)
         
         let celsiusTemp = Int(value)
         celsiusLabel.text = "\(celsiusTemp) Cº"
         
-        var convertedTempString = ""
-        #warning("switch case, convertedTempString = convertTempFrom(celsius: celsiusTemp)")
-    
+        let convertedTemps = convertTempFrom(celsius: celsiusTemp)
+        
+        switch tempSegment.selectedSegmentIndex {
+        case 0:
+            resultLabel.text = String(format: "%.f Fº", convertedTemps.fahrenheit)
+        case 1:
+            resultLabel.text = String(format: "%.f Kº", convertedTemps.kelvin)
+        default:
+            break
+        }
     }
     
-
     func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double) {
-        #warning("temp calculation logic")
-        return (0,0)
+        let fahrenheit = Double(celsius)*9/5+32
+        let kelvin = Double(celsius) + 273
+        return (fahrenheit, kelvin)
     }
-
 }
+
 
